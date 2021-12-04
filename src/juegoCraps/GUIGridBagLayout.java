79dac3b0fc8e43edc18fae71668e53d9f6035de3
EsperanzaGO <esperanza.olivo@correounivalle.edu.co>
2021-12-04 10:57:07 -0500

    start GUI

---
+++ src/juegoCraps/GUIGridBagLayout.java
@@ -0,0 +1,89 @@
+package juegoCraps;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GUIGridBagLayout extends JFrame {
+    private static final  String MENSAJE_INICIO = "Bienvenido a Craps \n"
+            + "Oprime el botón lanzar para iniciar el juego"
+            + "\n Si tu tiro de salida es 7 u 11 ganas con Natural"
+            + "\n Si tu tiro de salida es 2, 3  o 12 pierdes con Craps"
+            + "\n Si sacas cualquier otro valor establecerás el Punto."
+            + "\n Estado en punto podrás seguir lanzando los dados, "
+            + "\n pero ganarás si sacas nuevamente el valor del Punto"
+            + "\n sin que previamente hayas sacado 7.";
+
+    private Header headerProject;
+    private JLabel dado1, dado2;
+    private JButton lanzar, salir, ayuda;
+    private JPanel panelDados;
+    private ImageIcon imageDado;
+    private JTextArea resultados, mensajeSalida, resultadosDados;
+    private  JSeparator separator;
+    private Escucha escucha;
+    private ModelCraps modelCraps;
+
+    public GUIGridBagLayout(){
+        initGUI();
+
+        //Default JFrame configuration
+        this.setTitle("Juego Craps");
+        this.pack();
+        this.setResizable(true);
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public void initGUI(){
+        //Set up JFrame Container's Layout
+        this.getContentPane().setLayout(new GridBagLayout());
+        GridBagConstraints  constraints = new GridBagConstraints();
+
+        //Create Listener Object or Control Object
+        escucha = new Escucha();
+        modelCraps = new ModelCraps();
+        //Set up JComponents
+
+        headerProject = new Header("Mesa de Juego Craps", Color.BLACK);
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        constraints.gridwidth = 2;
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+
+        this.add(headerProject,constraints);
+    }
+
+    /**
+     * Main process of the Java program
+     * @param args Object used in order to send input data from command line when
+     *             the program is execute by console.
+     */
+    public static void main(String[] args){
+        EventQueue.invokeLater(() -> {
+            GUIGridBagLayout miProjectGUI = new GUIGridBagLayout();
+        });
+    }
+
+    private class Escucha implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            modelCraps.calcularTiro();
+            int[] caras = modelCraps.getCaras();
+            imageDado = new ImageIcon(getClass().getResource("/resources/"+caras[0]+".png"));
+            dado1.setIcon(imageDado);
+            imageDado = new ImageIcon(getClass().getResource("/resources/"+caras[1]+".png"));
+            dado2.setIcon(imageDado);
+
+            modelCraps.determinarJuego();
+
+            resultadosDados.setText(modelCraps.getEstadoToString()[0]);
+            mensajeSalida.setRows(4);
+            mensajeSalida.setText(modelCraps.getEstadoToString()[1]);
+
+        }
+    }
+}
